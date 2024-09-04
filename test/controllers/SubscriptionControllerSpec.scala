b36259091b98e67ffa48d8feaf7263851e078cbb
Rabina-Rai <174024275+Rabina-Rai@users.noreply.github.com>
2024-09-04 09:25:42 +0100

    [ODPR-1424] - SUB-16.Add processingDate

--- test/controllers/SubscriptionControllerSpec.scala
+++ test/controllers/SubscriptionControllerSpec.scala
@@ -26,10 +26,10 @@ import org.apache.pekko.Done
 import org.mockito.ArgumentMatchers.{any, eq as eqTo}
 import org.mockito.Mockito
 import org.mockito.Mockito.{times, verify, when}
-import org.scalatest.{BeforeAndAfterEach, OptionValues}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.{BeforeAndAfterEach, OptionValues}
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.mockito.MockitoSugar.mock
 import play.api.inject.bind
@@ -40,6 +40,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import uk.gov.hmrc.auth.core.AuthConnector
 
+import java.time.Instant
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -61,22 +62,18 @@ class SubscriptionControllerSpec
   }
 
   ".subscribe" - {
-
     "must return OK and the response detail" - {
-
       "when a subscription call was successful" in {
+        val app = new GuiceApplicationBuilder()
+          .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
+          .build()
 
-        val app =
-          new GuiceApplicationBuilder()
-            .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
-            .build()
-          
         val individual = IndividualContact(Individual("first", "last"), "email", None)
         val subscriptionRequest = SubscriptionRequest("userId", true, None, individual, None)
-        val subscriptionResponse = SubscribedResponse("dprs id")
+        val subscriptionResponse = SubscribedResponse("dprs id", Instant.parse("2024-03-17T09:30:47Z"))
         val payload = Json.obj(
           "id" -> "userId",
-          "gbUser"  -> true,
+          "gbUser" -> true,
           "primaryContact" -> Json.obj(
             "individual" -> Json.obj(
               "firstName" -> "first",
@@ -85,15 +82,11 @@ class SubscriptionControllerSpec
             "email" -> "email"
           )
         )
-        
+
         when(mockConnector.subscribe(any())(any())).thenReturn(Future.successful(subscriptionResponse))
 
         running(app) {
-
-          val request =
-            FakeRequest(routes.SubscriptionController.subscribe())
-              .withJsonBody(payload)
-
+          val request = FakeRequest(routes.SubscriptionController.subscribe()).withJsonBody(payload)
           val result = route(app, request).value
 
           status(result) mustEqual OK
@@ -104,12 +97,9 @@ class SubscriptionControllerSpec
     }
 
     "must return CONFLICT when the server returns duplicate submission" in {
-
-      val app =
-        new GuiceApplicationBuilder()
-          .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
-          .build()
-
+      val app = new GuiceApplicationBuilder()
+        .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
+        .build()
       val individual = IndividualContact(Individual("first", "last"), "email", None)
       val subscriptionRequest = SubscriptionRequest("userId", true, None, individual, None)
       val payload = Json.obj(
@@ -127,11 +117,8 @@ class SubscriptionControllerSpec
       when(mockConnector.subscribe(any())(any())).thenReturn(Future.successful(AlreadySubscribedResponse))
 
       running(app) {
-
-        val request =
-          FakeRequest(routes.SubscriptionController.subscribe())
-            .withJsonBody(payload)
-
+        val request = FakeRequest(routes.SubscriptionController.subscribe())
+          .withJsonBody(payload)
         val result = route(app, request).value
 
         status(result) mustEqual CONFLICT
@@ -140,12 +127,9 @@ class SubscriptionControllerSpec
     }
 
     "must return INTERNAL_SERVER_ERROR when the server returns an unexpected error" in {
-
-      val app =
-        new GuiceApplicationBuilder()
-          .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
-          .build()
-
+      val app = new GuiceApplicationBuilder()
+        .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
+        .build()
       val individual = IndividualContact(Individual("first", "last"), "email", None)
       val subscriptionRequest = SubscriptionRequest("userId", true, None, individual, None)
       val payload = Json.obj(
@@ -163,11 +147,8 @@ class SubscriptionControllerSpec
       when(mockConnector.subscribe(any())(any())).thenReturn(Future.successful(UnexpectedResponse("")))
 
       running(app) {
-
-        val request =
-          FakeRequest(routes.SubscriptionController.subscribe())
-            .withJsonBody(payload)
-
+        val request = FakeRequest(routes.SubscriptionController.subscribe())
+          .withJsonBody(payload)
         val result = route(app, request).value
 
         status(result) mustEqual INTERNAL_SERVER_ERROR
@@ -176,19 +157,16 @@ class SubscriptionControllerSpec
     }
 
     "must fail" - {
-
       "when a request to the backend fails" in {
+        val app = new GuiceApplicationBuilder()
+          .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
+          .build()
 
-        val app =
-          new GuiceApplicationBuilder()
-            .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
-            .build()
-          
         when(mockConnector.subscribe(any())(any())).thenReturn(Future.failed(new Exception("foo")))
-        
+
         val payload = Json.obj(
           "id" -> "userId",
-          "gbUser"  -> false,
+          "gbUser" -> false,
           "primaryContact" -> Json.obj(
             "individual" -> Json.obj(
               "firstName" -> "first",
@@ -197,10 +175,9 @@ class SubscriptionControllerSpec
             "email" -> "email"
           )
         )
-        
-        val request =
-          FakeRequest(routes.SubscriptionController.subscribe())
-            .withJsonBody(payload)
+
+        val request = FakeRequest(routes.SubscriptionController.subscribe())
+          .withJsonBody(payload)
 
         running(app) {
           route(app, request).value.failed
@@ -210,21 +187,16 @@ class SubscriptionControllerSpec
   }
 
   ".updateSubscription" - {
-
     "must return OK" - {
-
       "when an update subscription call was successful" in {
-
-        val app =
-          new GuiceApplicationBuilder()
-            .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
-            .build()
-
+        val app = new GuiceApplicationBuilder()
+          .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
+          .build()
         val individual = IndividualContact(Individual("first", "last"), "email", None)
         val subscriptionRequest = SubscriptionRequest("userId", true, None, individual, None)
         val payload = Json.obj(
           "id" -> "userId",
-          "gbUser"  -> true,
+          "gbUser" -> true,
           "primaryContact" -> Json.obj(
             "individual" -> Json.obj(
               "firstName" -> "first",
@@ -236,12 +208,10 @@ class SubscriptionControllerSpec
 
         when(mockConnector.updateSubscription(any())(any())).thenReturn(Future.successful(Done))
 
-        val request =
-          FakeRequest(routes.SubscriptionController.updateSubscription())
-            .withJsonBody(payload)
+        val request = FakeRequest(routes.SubscriptionController.updateSubscription())
+          .withJsonBody(payload)
 
         running(app) {
-
           val result = route(app, request).value
 
           status(result) mustEqual OK
@@ -251,19 +221,16 @@ class SubscriptionControllerSpec
     }
 
     "must fail" - {
-
       "when a request to the backend fails" in {
-
-        val app =
-          new GuiceApplicationBuilder()
-            .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
-            .build()
+        val app = new GuiceApplicationBuilder()
+          .overrides(bind[SubscriptionConnector].toInstance(mockConnector))
+          .build()
 
         when(mockConnector.updateSubscription(any())(any())).thenReturn(Future.failed(new Exception("foo")))
 
         val payload = Json.obj(
           "id" -> "userId",
-          "gbUser"  -> false,
+          "gbUser" -> false,
           "primaryContact" -> Json.obj(
             "individual" -> Json.obj(
               "firstName" -> "first",
@@ -274,10 +241,8 @@ class SubscriptionControllerSpec
         )
 
         running(app) {
-
-          val request =
-            FakeRequest(routes.SubscriptionController.updateSubscription())
-              .withJsonBody(payload)
+          val request = FakeRequest(routes.SubscriptionController.updateSubscription())
+            .withJsonBody(payload)
 
           route(app, request).value.failed
         }
@@ -286,25 +251,19 @@ class SubscriptionControllerSpec
   }
 
   ".get" - {
-    
     "must return the user's subscription info when the user is authenticated and the server returns OK" in {
-
-      val app =
-        new GuiceApplicationBuilder()
-          .overrides(
-            bind[SubscriptionConnector].toInstance(mockConnector),
-            bind[AuthAction].toInstance(new FakeAuthAction)
-          )
-          .build()
-        
+      val app = new GuiceApplicationBuilder().overrides(
+          bind[SubscriptionConnector].toInstance(mockConnector),
+          bind[AuthAction].toInstance(new FakeAuthAction)
+        )
+        .build()
       val contact = OrganisationContact(Organisation("name"), "email", None)
       val subscriptionInfo = SubscriptionInfo(dprsId, true, None, contact, None)
       when(mockConnector.get(eqTo(dprsId))(any())).thenReturn(Future.successful(subscriptionInfo))
-      
+
       val request = FakeRequest(GET, routes.SubscriptionController.get().url)
 
       running(app) {
-
         val result = route(app, request).value
         status(result) mustEqual OK
       }
