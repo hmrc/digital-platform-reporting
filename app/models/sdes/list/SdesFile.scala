3b91ac0652201f5cb6a127cd7bf59a17f486525e
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-10-26 14:19:10 +0100

    Add listFiles to SdesConnector

---
+++ app/models/sdes/list/SdesFile.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.sdes.list
+
+import models.urlFormat
+import play.api.libs.json.{Json, OFormat}
+
+import java.net.URL
+
+final case class SdesFile(
+                           fileName: String,
+                           fileSize: Long,
+                           downloadUrl: URL,
+                           metadata: Seq[MetadataValue]
+                         )
+
+object SdesFile {
+
+  given OFormat[SdesFile] = Json.format
+}
+
+final case class MetadataValue(metadata: String, value: String)
+
+object MetadataValue {
+
+  given OFormat[MetadataValue] = Json.format
+}
